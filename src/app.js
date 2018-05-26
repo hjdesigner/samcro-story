7cc84862afab51b29802fc25f2f9d5ad5cfbc3d2
Henrique Melanda <hjdesigner23@gmail.com>
2018-05-26 18:48:57 -0300

    Initial commit with project base

---
+++ src/app.js
@@ -0,0 +1,19 @@
+'use strict'
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+    }
+  }
+  render () {
+    return (
+      <div>
+        <h1>Base project</h1>
+      </div>
+    )
+  }
+}
+
+export default App
